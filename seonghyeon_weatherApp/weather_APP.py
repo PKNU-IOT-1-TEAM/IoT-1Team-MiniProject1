876cbd65939f494a76ceaadf7bb9abe75aecfe9c
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-03-19 23:26:06 +0900

    앱에 사용할 DB 데이터 쿼리문

--- seonghyeon_weatherApp/weather_APP.py
+++ seonghyeon_weatherApp/weather_APP.py
@@ -11,7 +11,7 @@ today = datetime.today().strftime("%Y%m%d") # 특수문자 제거
 tomorrow = (date.today() + timedelta(days=1)).strftime("%Y%m%d")
 day_after_tomorrow = (date.today() + timedelta(days=2)).strftime("%Y%m%d")
 
-select_data_list = ('fcstDate', 'fcstTime', 'POP', 'PTY', 'REH', 'SKY', 'TMP', 'WSD')
+select_data_list = ['fcstDate', 'fcstTime', 'POP', 'PTY', 'REH', 'SKY', 'TMP', 'WSD']
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -19,7 +19,7 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):    
         super(MainWindow, self).__init__(*args, **kwargs)
         #Load the UI Page
-        uic.loadUi('./seonghyeon_weatherApp/weatherApp.ui', self)
+        uic.loadUi('./seonghyeon_weatherApp/weatherApp.ui')
         self.plot([1,2,3,4,5,6,7,8,9,10], [30,32,34,32,33,31,29,32,35,45])
 
 
@@ -36,19 +36,17 @@ class MainWindow(QtWidgets.QMainWindow):
             charset = 'utf8'
         )
         cur = con.cursor()
-
-        get_index_result = f'''SELECT DISTINCT {select_data_list} 
-                                 FROM weather 
-                                WHERE (fcstDate = {today} and fcstTime = 1200) 
-                                   or (fcstDate = {tomorrow} and fcstTime = 1200) 
-                                   or (fcstDate = {day_after_tomorrow} and fcstTime = 1200);'''
-        
-        for item in select_data_list:
-            
-        cur.execute(get_index_result)
-        print(cur.fetchall())
+        item_list = []
+        for item in range(len(select_data_list)):
+            get_index_result = f'''SELECT {select_data_list[item]} 
+                                     FROM weather 
+                                    WHERE (fcstDate = {today} and fcstTime = 1200) 
+                                       or (fcstDate = {tomorrow} and fcstTime = 1200) 
+                                       or (fcstDate = {day_after_tomorrow} and fcstTime = 1200);'''
+            item_list.append(cur.execute(get_index_result))
+            print(cur.fetchall())
         con.close()
-
+        print(item_list)
 def main():
     MainWindow.initDB(sys.argv)
     app = QtWidgets.QApplication(sys.argv)
