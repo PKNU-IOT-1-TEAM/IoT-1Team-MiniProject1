43b6eda84f5eef8b7f2bf0734b52630ca28ef7c0
JuHyunLee99 <123914434+JuHyunLee99@users.noreply.github.com>
2023-03-22 00:44:54 +0900

    FcstDB 완료

--- juhyunMini/FcstDB.py
+++ juhyunMini/FcstDB.py
@@ -10,41 +10,55 @@ import pymysql
 import schedule
 import time
 
-# 처음 db 삽입 후 설정한 시간되면 자동으로 업데이트
+# 개인DB
+# conn = pymysql.connect(host='localhost', user = 'root', password='12345',
+                        # db = 'miniproject', charset='utf8')
+# 성현DB
+# conn = pymysql.connect(host='210.119.12.66', user = 'root', password='12345',
+#                     db = 'miniproject01', charset='utf8')
+
+
+# 1. 처음 실행시 현재 시각기준으로 DB삽입
+# 2. API 기준 시간되면 자동으로 DB 업데이트
+# 3. TEST를 위한 임의 시각도 가능
 class FcstDB:
+    # 처음 실행시 현재 시각기준으로 DB 삽입
     def __init__(self) -> None:
         self.insertDB(1)
         print(f'[{dt.now()}] 기상DB 생성 완료')
 
     def updateDB(self, mode, *setDate):
-        if mode == 0:
-            allFcstData = self.newData(0)    # API  0: 자동 시간
-        if mode == 2:
-             allFcstData = self.newData(2, setDate[0], setDate[1])
-        # DB
+        if mode == 0:   # Mode: 0 설정한 시간에 API
+            allFcstData = self.newData(mode) 
+        if mode == 2:   # Mode: 2 업데이트동작 TEST를 위해 임의로 설정한 시간기준 API
+             allFcstData = self.newData(mode, setDate[0], setDate[1])
+
+        # DB Connection
         conn = pymysql.connect(host='localhost', user = 'root', password='12345',
                                db = 'miniproject', charset='utf8')
         cur = conn.cursor()    # Connection으로부터 Cursor 생성
+        # SELECT 쿼리문
         query = '''SELECT Idx, fcstDate, fcstTime
                      FROM weather'''
         cur.execute(query) 
         exits = cur.fetchall()
         # print(currentDB)
         
-        # API, DB 데이터 비교하여 업데이트 OR 삽입
+        # API, DB 데이터 비교하여 업데이트 또는 삽입
         for fcstData in allFcstData:
             change = False
-            for exit in exits:                
+            for exit in exits:      # DB에 이미 존재하는 데이터일 경우 업데이트       
                 if fcstData['fcstDate'] == str(exit['fcstDate']).replace('-', '') and \
                    str(fcstData['fcstTime'])+'00' == ((lambda x: '0' + x if len(x) < 8 else x)(str(exit['fcstTime']))).replace(':', ''):
                         self.updateQuery(fcstData, cur)
                         change = True
                         break
-            if change == False:
+            if change == False:     # DB에 이미 존재하지 않을 경우 데이터일 경우 삽입
                  self.insertQuery(fcstData, cur)
             conn.commit()
         conn.close()    
 
+    # 업데이트 쿼리문
     def updateQuery(self, fcstData, cur):
         if 'TMN' in fcstData.keys():
              query = '''UPDATE weather
@@ -67,7 +81,8 @@ class FcstDB:
             cur.execute(query, (fcstData['TMP'], fcstData['VEC'], fcstData['WSD']
                                , fcstData['SKY'], fcstData['PTY'], fcstData['POP']
                                , fcstData['PCP'], fcstData['REH'], fcstData['SNO'], fcstData['fcstDate'], fcstData['fcstTime']))
-              
+
+    # 삽입 쿼리문        
     def insertQuery(self, fcstData, cur):
         if 'TMN' in fcstData.keys():
                 query = '''INSERT INTO weather (fcstDate, fcstTime, TMP, VEC, WSD, SKY, PTY, POP, PCP, REH, SNO, TMN)
@@ -89,49 +104,49 @@ class FcstDB:
                                         fcstData['PCP'], fcstData['REH'], fcstData['SNO']))
 
     def insertDB(self, mode):
-        # 주현집 host
+        # DB Connection
         conn = pymysql.connect(host='localhost', user = 'root', password='12345',
                                db = 'miniproject', charset='utf8')
-        # 성현DB
-        # conn = pymysql.connect(host='210.119.12.66', user = 'root', password='12345',
-        #                     db = 'miniproject01', charset='utf8')
         cur = conn.cursor()    # Connection으로부터 Cursor 생성
 
+        # TRUNCATE 쿼리문
         query = '''TRUNCATE weather'''
         cur.execute(query)
 
-        allFcstData = self.newData(mode)   # API 날짜 mode = 1 현재시간
+        # API 불러와서 INSERT쿼리문 실행
+        allFcstData = self.newData(mode)
         for fcstData in allFcstData:
             self.insertQuery(fcstData, cur)                
             conn.commit()
         conn.close()
         print('DB 데이터 삽입')
     
+    # API
     def newData(self, mode, *setDate):
         # 검색 날짜 설정
         baseDate = f'{dt.now().date().strftime("%Y%m%d")}'
-        if mode == 1:    # 초기화 첫 시간으로 데이터 삽입
+        if mode == 1:    # mode:1 첫 실행시 현재 시각 기준으로 데이터 삽입
             nowTime = dt.now().time()
             API_TIME = [datetime.time(2,10), datetime.time(5,10), datetime.time(8,10),
                         datetime.time(11,10), datetime.time(14,10), datetime.time(17,10),
                         datetime.time(20,10), datetime.time(23,10)]
-
+            # 현재시각을 API 검색 시각으로 변환
             for i, time in enumerate(API_TIME[::-1]):
                 if nowTime >= time:
                     baseTime = f'{time.strftime("%H")}00'
                     break
-                elif nowTime < API_TIME[0]:
+                elif nowTime < API_TIME[0]: # 00시 ~ 2시10분 사이는 검색시간을 전날 23시로 설정 
                     baseDate = f'{(dt.now().date() - td(days=1)).strftime("%Y%m%d")}'
                     baseTime = f'{API_TIME[i-1].strftime("%H")}00'
                     break
-        elif mode == 2:
+        elif mode == 2: # mode:2 TEST
              baseDate = setDate[0]
              baseTime = setDate[1]
 
-        else:   # 정해진 시간에 자동 업데이트  
+        else:   # mode: 0 정해진 시간에 자동 업데이트  
             baseTime = f'{dt.now().time().strftime("%H")}00'
 
-        # 실제 데이터 가져오기
+        # 실제 API 데이터 가져오기
         api = FcstAPI() # API 객체 생성
         try:
             result = api.getDataPortalSearch(baseDate, baseTime)
@@ -140,34 +155,35 @@ class FcstDB:
         except Exception as e:
                 print('찾는 데이터가 없습니다.')
 
+        # 날짜,시간 별로 모으기 위해 날짜, 시간 변화에 대한 변수 선언
         changeDate = newFcstData[0]['fcstDate']
         changeTime = newFcstData[0]['fcstTime']
-        
-        fcstData = dict()
-        allFcstData = []
+        fcstData = dict()   # FOR문 돌면서 날짜,시간별 모든 요소 데이터 담기위한 사전
+        allFcstData = []    # 전체 데이터 리스트
         CATEGORY = ['TMP', 'VEC', 'WSD', 'SKY', 'PTY', 'POP', 'PCP', 'REH', 'SNO', 'TMN', 'TMM']
         # print(newFcstData)
         for item in newFcstData:
             print(item['fcstDate'], item['fcstTime'])
-            del item['baseDate'], item['baseTime'], item['nx'], item['ny']
+            del item['baseDate'], item['baseTime'], item['nx'], item['ny']  # 필요없는 것 삭제
 
-            for category in CATEGORY:           
+            for category in CATEGORY:
+            # item 딕셔너리 키,값 정리           
                 if item['category'] == category:
-                    item[f'{category}'] = item['fcstValue']
+                    item[f'{category}'] = item['fcstValue'] # (key:카테고리 명/ value: 수치) 데이터 추가
                     break
                 
-            del item['category'], item['fcstValue']
+            del item['category'], item['fcstValue'] # (key:카테고리, key: 수치) 데이터 삭제
 
             if changeDate == item['fcstDate'] and changeTime == item['fcstTime']:
-                fcstData = fcstData|item
-            else:
-                allFcstData.append(fcstData)
-                fcstData = dict()
-                fcstData = item
-                changeDate = item['fcstDate']
-                changeTime = item['fcstTime']
+                fcstData = fcstData|item    # 날짜, 시간 같으면 정리한 item을 fcstDate에 쌓기
+            else:   # 날짜,시간 다르면 
+                allFcstData.append(fcstData)    # allFcstData(전체 데이터)에 같은 시각의 데이터를 모은 fcstDate 추가
+                fcstData = dict()   # 다음 시각의 데이터를 모으기 위해 초기화후 item 대입
+                fcstData = item 
+                changeDate = item['fcstDate']   # 날짜 바꾸기
+                changeTime = item['fcstTime']   # 시간 바꾸기
 
-        allFcstData.append(fcstData)
+        allFcstData.append(fcstData)    # 마지막 날짜시각 데이터도 allFcstDate에 추가
         # print(allFcstData)
         return allFcstData
     
--- juhyunMini/FcstDB2.py
+++ juhyunMini/FcstDB2.py
@@ -12,7 +12,7 @@ import time
 # pandas
 import pandas as pd
 
-# 주현 host
+# 개인
 # conn = pymysql.connect(host='localhost', user = 'root', password='12345',
 #                         db = 'miniproject', charset='utf8')
 # 성현DB
@@ -20,31 +20,27 @@ import pandas as pd
 #                     db = 'miniproject01', charset='utf8')
 
 
-# API, DB 데이터 삽입, 업데이트
+# 1. 처음 실행시 어제 오전 2시부터 현재 시각까지의 API 데이터 DB업데이트
+# 2. API 기준시간에 자동으로 DB 업데이트
 class FcstDB:
-
     def __init__(self) -> None:
-        self.mode, self.i = -1, 0
-        # 어제 데이터
-        baseDate, baseTime = self.baseDateTime()
-        self.insertDB(baseDate, baseTime)
+        # 어제 API 데이터 DB삽입, 업데이트
+        for pasttime in range(8):
+            baseDate, baseTime = self.baseDateTime(-1, pasttime)    # 어제날짜, 시간
+            if pasttime == 0:   # 첫 데이터는 DB Insert
+                self.insertDB(baseDate, baseTime)
+            else:   # DB Update
+                self.updateDB(baseDate, baseTime)
 
-        for _ in range(7):
-            baseDate, baseTime = self.baseDateTime()
-            self.updateDB(baseDate, baseTime)
-
-        # 현재 시각까지 데이터
-        self.mode, self.i = 0, 0
-        num = self.baseDateTime()
-        if num == -1:
+        # 현재 시각까지 API 데이터 DB 업데이트
+        num = self.baseDateTime(0)   # 반복 횟수 받기
+        if num == -1:   # 현재시간이 오전 2시 이전이면 -1 -> API 업데이트할 거 없음
             pass
         else:
-            self.mode = 1
-            for _ in range(num):
-                baseDate, baseTime = self.baseDateTime()
+            for pasttime in range(num):
+                baseDate, baseTime = self.baseDateTime(1, pasttime)
                 self.updateDB(baseDate, baseTime)
-        print('DB 초기화 완료')
-        mode = 2             
+        print('DB 초기화 완료')          
 
     def insertDB(self, baseDate, baseTime):        
         # DB connection
@@ -58,7 +54,7 @@ class FcstDB:
         
         # API 데이터
         allFcstData = self.newData(baseDate, baseTime)
-        # DB에 API 데이터 삽입
+        # DB에 API 데이터 Insert
         for fcstData in allFcstData:
             self.insertQuery(fcstData, cur)                
             conn.commit()
@@ -103,12 +99,13 @@ class FcstDB:
 
         # API 데이터
         allFcstData = self.newData(baseDate, baseTime)
-        # API, DB 데이터 비교하여 업데이트 OR 삽입
+        # API, DB 데이터 비교하여 Update 또는 Insert
         for fcstData in allFcstData:
+            # 날짜, 시간이 같은 데이터 행 조회
             exitdb = df[(df['Date'] == fcstData['fcstDate']) & (df['Time'] == fcstData['fcstTime'])]
-            if len(exitdb):
+            if len(exitdb): # 있다면 Update
                 self.updateQuery(fcstData, cur)
-            else:
+            else:   # 없다면 Insert
                 self.insertQuery(fcstData, cur)
             conn.commit()
         conn.close()
@@ -137,40 +134,38 @@ class FcstDB:
                                , fcstData['SKY'], fcstData['PTY'], fcstData['POP']
                                , fcstData['PCP'], fcstData['REH'], fcstData['SNO'], fcstData['fcstDate'], fcstData['fcstTime']))
     
-    def baseDateTime(self):
+    def baseDateTime(self, mode, *pasttime):
         PARA_TIME = ['0200','0500', '0800', '1100', '1400', '1700', '2000', '2300']
         API_TIME = [datetime.time(2,10), datetime.time(5,10), datetime.time(8,10),
                             datetime.time(11,10), datetime.time(14,10), datetime.time(17,10),
                             datetime.time(20,10), datetime.time(23,10)]
         
-        # 어제 오전 2시부터 오후 23시까지 8번 업데이트
-        if self.mode == -1:           
+        # 어제 오전 2시부터 오후 23시까지
+        if mode == -1:           
             baseDate = f'{(dt.now().date() - td(days=1)).strftime("%Y%m%d")}'
-            baseTime = PARA_TIME[self.i]
-            # print(PARA_TIME[self.i])
-            self.i += 1    
-            return (baseDate, baseTime)
-
-        elif self.mode == 0:
+            baseTime = PARA_TIME[pasttime[0]]      
+        
+        # 오늘중 현재시간까지의 API 기준시간 갯수
+        elif mode == 0:   
             nowTime = dt.now().time()
             for i, time in enumerate(API_TIME[::-1]):
                 if nowTime >= time:
                     # baseTime = PARA_TIME[i]
                     return (len(PARA_TIME)-i)
-                elif nowTime < API_TIME[0]:
+                elif nowTime < API_TIME[0]: # 현재 시각이 오전 2시이면 오늘 업데이트할 것 없음. 
                     return -1
                 
-        elif self.mode == 1:
+        # 현재시간이전의 API 기준시간
+        elif mode == 1:
             baseDate = f'{dt.now().date().strftime("%Y%m%d")}'
-            baseTime = PARA_TIME[self.i]
-            # print(PARA_TIME[self.i])
-            self.i += 1    
-            return (baseDate, baseTime)
+            baseTime = PARA_TIME[pasttime[0]]
         
-        elif self.mode == 2:
+        # 자동 업데이트할 API 기준시간
+        elif mode == 2:
             baseDate = f'{dt.now().date().strftime("%Y%m%d")}'
             baseTime = f'{dt.now().time().strftime("%H")}00'
-            return (baseDate, baseTime)
+
+        return (baseDate, baseTime)
                        
     def newData(self, baseDate, baseTime):
         # 실제 API 데이터 가져오기
@@ -182,6 +177,7 @@ class FcstDB:
         except Exception as e:
                 print('찾는 데이터가 없습니다.')
 
+        # 시각 별로 모으기 위해 날짜, 시간 변화에 대한 변수 선언
         changeDate = newFcstData[0]['fcstDate']
         changeTime = newFcstData[0]['fcstTime']
         
@@ -190,31 +186,36 @@ class FcstDB:
         CATEGORY = ['TMP', 'VEC', 'WSD', 'SKY', 'PTY', 'POP', 'PCP', 'REH', 'SNO', 'TMN', 'TMM']
 
         for item in newFcstData:
+            # 필요없는 것 삭제
             del item['baseDate'], item['baseTime'], item['nx'], item['ny']
 
-            for category in CATEGORY:           
+            for category in CATEGORY:
+                # item 딕셔너리 키,값 정리           
                 if item['category'] == category:
+                    # (key:카테고리 명/ value: 수치) 추가
                     item[f'{category}'] = item['fcstValue']
                     break
-                
-            del item['category'], item['fcstValue']
+            # (key:카테고리, key: 수치) 데이터 삭제    
+            del item['category'], item['fcstValue'] 
 
+            # 날짜, 시간 같으면 정리한 item을 fcstDate에 쌓기
             if changeDate == item['fcstDate'] and changeTime == item['fcstTime']:
                 fcstData = fcstData|item
-            else:
+            # 날짜,시간 다르면 같은 시각의 데이터들을 모은
+            else:   # 딕셔너리fcstDate를 allFcstData에 추가하고 초기화하고 item 담기
                 fcstData['fcstTime'] = fcstData['fcstTime'] + '00'  # TIME : 000000
                 allFcstData.append(fcstData)
                 fcstData = dict()
                 fcstData = item
-                changeDate = item['fcstDate']
-                changeTime = item['fcstTime']
+                changeDate = item['fcstDate']   # 날짜 변경
+                changeTime = item['fcstTime']   # 시간 변경
 
         fcstData['fcstTime'] = fcstData['fcstTime'] + '00'        
-        allFcstData.append(fcstData)
+        allFcstData.append(fcstData)    # 마지막 날짜시각 데이터 allFcstDate에 추가
         return allFcstData
 
-    def autoUpdateDB(self):
-        baseDate, baseTime = self.baseDateTime()
+    def autoUpdateDB(self): # 자동 DB 업데이트
+        baseDate, baseTime = self.baseDateTime(2)
         self.updateDB(baseDate, baseTime)
 
 
