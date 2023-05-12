0ecad7b0cf9ed9b73aed2bb910f3a474f6cc83af
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-05-12 11:37:50 +0900

    2023.05.12

--- seonghyeon_weatherApp/C#_Weather/C#_WeatherApp/legalDong/obj/Debug/net7.0-windows/App.g.i.cs
+++ seonghyeon_weatherApp/C#_Weather/C#_WeatherApp/legalDong/obj/Debug/net7.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace legalDong {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace legalDong {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
         public static void Main() {
             legalDong.App app = new legalDong.App();
             app.InitializeComponent();
