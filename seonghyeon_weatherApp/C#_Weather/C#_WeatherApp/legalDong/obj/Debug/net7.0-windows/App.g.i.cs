156076afe7683c3cd03755e1bbb18d957236e4c4
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-05-15 21:46:03 +0900

    테스트

--- seonghyeon_weatherApp/C#_Weather/C#_WeatherApp/legalDong/obj/Debug/net7.0-windows/App.g.i.cs
+++ seonghyeon_weatherApp/C#_Weather/C#_WeatherApp/legalDong/obj/Debug/net7.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace legalDong {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace legalDong {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/legalDong;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/legalDong;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace legalDong {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
         public static void Main() {
             legalDong.App app = new legalDong.App();
             app.InitializeComponent();
